d2a87f6c92d56d50f98609def3e7214e23531b3e
sherbracewell <70412782+sherbracewell@users.noreply.github.com>
2021-02-17 23:31:28 -0500

    Worked on Player Controller script

---
+++ Team Project 1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameOver = false;
+    public Text gameOverText;
+
+    void Start()
+    {
+        gameOverText.text = "";
+    
+    }
+
+    public void gameEnded()
+        {
+          if (gameOver == true)
+          
+          {
+             gameOverText.gameObject.SetActive(true);
+             gameOverText.text = "Game Over!";
+          }
+
+        }
+}
